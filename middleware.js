facb41738629163c2ece5db789e0ad0607a7341a
gamer-1478 <aayushgarg2006@gmail.com>
2023-10-27 01:27:31 +0530

    final

--- middleware.js
+++ middleware.js
@@ -6,4 +6,8 @@ export async function middleware(req) {
     if (req.nextUrl.pathname.toLowerCase() == '/satan') {
         return NextResponse.redirect(new URL('https://cdn.discordapp.com/attachments/983003838304886854/1167164721561083965/SlashTheBrand.wav?ex=654d21da&is=653aacda&hm=47b0de2ee7989f89552037b3455e7584dd6708bcf423ac7cb15c5c97f24da915&', req.url));
     }
+    if (req.nextUrl.pathname.toLowerCase() =='/jackbuckner'){
+        return NextResponse.redirect(new URL('/level15hiddentexttest', req.url));
+
+    }
 }
